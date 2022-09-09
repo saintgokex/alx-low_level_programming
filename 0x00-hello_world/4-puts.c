f4a5666a6d1585aaab9ad87616de91a7e441dfc0
saintgokex <drgokex@gmail.com>
2022-09-08 20:12:49 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  *
  * return always (0)
  */
-int main(void)//this specifies there are no arguments taken by main
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
