599975d86b865272d5f3d8f20b384f6921ec7356
saintgokex <drgokex@gmail.com>
2022-09-08 19:46:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-#include <stdio.h> \n int main (void) \n {\n int puts ("Programming is like building a multilingual puzzle") \n return (0) \n }
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return always (0)
+ */
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
