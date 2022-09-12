2a68713a0c07c52e0604d856fbdfb5239aed31fa
saintgokex <drgokex@gmail.com>
2022-09-12 09:35:13 -0700

    alphabet upper and lower

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints a-zA-Z
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	return('\n');
+
+	return (0);
+}
