77542ece2bede93dc15d9ae0d11f92c46db6a3ac
saintgokex <drgokex@gmail.com>
2022-09-12 08:30:26 -0700

    done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%s %d is ", last, n, n, % 10);
+	printf("%s %d and is ", last, n, n % 10);
 	if (n % 10 > 5)
 	{
-		printf("is greater than 5\n");
+		printf("greater than 5\n");
 	}
 	else if (n % 10 == 0)
 	{
