ceee9d0a84d0e1cc1d850876eb5b85f055ef5902
saintgokex <drgokex@gmail.com>
2022-09-12 10:42:09 -0700

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints 0-10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= ; '10'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
