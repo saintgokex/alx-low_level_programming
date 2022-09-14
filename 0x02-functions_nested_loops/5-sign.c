8ff33c763435fe8d4938d90c2c781a1ccc92f086
saintgokex <drgokex@gmail.com>
2022-09-14 14:28:40 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - prints signs
+ * @n: type int character
+ * Return: Always 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+
+	}
+	else
+	{
+		_putcharI('-');
+		return (-1);
+
+
+	}
+
