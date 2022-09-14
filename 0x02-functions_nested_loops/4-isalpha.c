f1d0296f1e4a6cdf98aa98d8542475c5155bd20a
saintgokex <drgokex@gmail.com>
2022-09-14 13:40:16 -0700

    space fixed

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 /**
  * _isalpha - checks if c is an alphabet
  * @c: char type
@@ -7,12 +7,14 @@
 int _isalpha(int c)
 {
 	char c;
+	{
 	if (c < 91 && c > 4)
 		return (1);
 	else if (c < 123 && c > 96)
 		return (1);
 	else
 		return (0);
-
+	}
+	_putchar('\n');
 
 }
