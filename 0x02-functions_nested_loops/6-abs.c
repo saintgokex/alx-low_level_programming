abb619051ea07dd50d42056fcfb3a0c3ed8647f0
saintgokex <drgokex@gmail.com>
2022-09-14 14:59:44 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - compute absolute value
+ * @n: int type number
+ * Return: absolute value of @n
+ */
+int abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (b);
+
+}
