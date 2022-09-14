a543ab8a0c6020f281cf2581f06ea3a06d5bad7c
saintgokex <drgokex@gmail.com>
2022-09-14 11:11:22 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+ * main - Entry point
+ * Description: writes a to z
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z');
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
