3c6f6a856e45eb39844d6eb8f8be4a442d53b1e6
saintgokex <drgokex@gmail.com>
2022-09-14 13:36:48 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ * _isalpha - checks if c is an alphabet
+ * @c: char type
+ * Return: 1 if it's an alphabet, 0 if not
+ */
+int _isalpha(int c)
+{
+	char c;
+	if (c < 91 && c > 4)
+		return (1);
+	else if (c < 123 && c > 96)
+		return (1);
+	else
+		return (0);
+
+
+}
