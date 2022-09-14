0a55fd2f4bcdc22101b4f310db8a6cea4a3c8973
saintgokex <drgokex@gmail.com>
2022-09-14 12:30:52 -0700

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet in lowercase 10 times using putchar
+ *
+ * Return: Always (0)
+ */
+void print_alphabet_x10(void)
+{
+
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++
+	}
+}
