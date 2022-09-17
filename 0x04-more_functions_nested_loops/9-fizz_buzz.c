4e0eac5888fdc2e073ec132de72e95a75e1c29cf
saintgokex <drgokex@gmail.com>
2022-09-16 18:09:25 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,24 +8,25 @@ int main(void)
 {
 	int n;
 
-	for ("n = 0; n <= 100; n++", ' ')
+	for ("n = 0; n <= 100; n++")
 	{
 		if (n % 3 == '0')
 		{
-			printf("Fizz", ' ');
+			printf("Fizz");
 		}
 		else if (n % 5 == '0')
 		{
-			printf("Buzz", ' ');
+			printf("Buzz");
 		}
 		else if (n % 3 == '0' && n % 5 == '0')
 		{
-			printf("FizzBuzz", ' ');
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d", ' ')
+			printf("%d", n);
 		}
 	}
+	printf("\n");
 	return (0);
 }
