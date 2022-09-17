912f5f6bee2eb4bec7dd720fe5190bc64731dbba
saintgokex <drgokex@gmail.com>
2022-09-16 18:51:40 -0700

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,17 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0)
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf(" Fizz");
+			printf("FizzBuzz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		}
-		else if ((n % 3 == 0) && (n % 5 == 0))
+		else if (n % 3 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("Fizz");
 		}
 		else
 		{
