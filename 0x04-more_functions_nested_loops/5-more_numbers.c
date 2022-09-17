b0086140162b902aee2d909faff4dbf631627604
saintgokex <drgokex@gmail.com>
2022-09-16 17:07:39 -0700

    typo fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * more_numners - prints the numbers, from 0 to 14, 10 times
+ * more_numbers - prints the numbers, from 0 to 14, 10 times
  *
  * Return: from 0 to 14, 10 times, followed by a new line.
  */
