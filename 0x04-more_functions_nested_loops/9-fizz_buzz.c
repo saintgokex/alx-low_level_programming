36c91c19c6abcaa659bd0c3631e1d49d0817abb7
saintgokex <drgokex@gmail.com>
2022-09-16 18:35:27 -0700

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 /**
  * main - print buzz, fizz, for multiples of 5 and 3
- *
+ * @n - int type number
  * Return: Always 0
  */
 int main(void)
 {
 	int n;
 
-	n = 1;
-	printf("%d", n);
-	for (n = 2; n <= 100; n++)
+	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0)
 		{
