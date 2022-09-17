6ae3a7a522ad128e57a352feb517338842c92f19
saintgokex <drgokex@gmail.com>
2022-09-16 18:03:23 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - print number 0 to 100
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for ("n = 0; n <= 100; n++", ' ')
+	{
+		if (n % 3 == '0')
+		{
+			printf("Fizz", ' ');
+		}
+		else if (n % 5 == '0')
+		{
+			printf("Buzz", ' ');
+		}
+		else if (n % 3 == '0' && n % 5 == '0')
+		{
+			printf("FizzBuzz", ' ');
+		}
+		else
+		{
+			printf("%d", ' ')
+		}
+	}
+	return (0);
+}
