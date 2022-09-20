edbb94f048797b4622844a70702962a1dcdc94b4
saintgokex <drgokex@gmail.com>
2022-09-19 19:03:17 -0700

    done

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -7,7 +7,7 @@
  * Return: on success 1.
  * On error, -1 is returned, and errno is set appropriately
  */
-int _putchar(int *n)
+int _putchar(int *c)
 {
-	return (write(1, &n, 1));
+	return (write(1, &c, 1));
 }
