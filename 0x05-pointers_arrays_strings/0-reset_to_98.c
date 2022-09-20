f597a19064774871c9147f88b24393ad8634aacf
saintgokex <drgokex@gmail.com>
2022-09-19 17:57:59 -0700

    errors fixed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,14 +6,7 @@
  */
 void reset_to_98(int *n)
 {
-	int n;
-	int *p;
-
-	n = 402;
-	p = &n;
-	_putchar(n);
-
-	*p = 98;
+	*n = 98;
 	_putchar(&n);
 
 }
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -7,7 +7,7 @@
  * Return: on success 1.
  * On error, -1 is returned, and errno is set appropriately
  */
-int _putchar(char c)
+int _putchar(int n)
 {
-	return (write(1, &c, 1));
+	return (write(1, &n, 1));
 }
