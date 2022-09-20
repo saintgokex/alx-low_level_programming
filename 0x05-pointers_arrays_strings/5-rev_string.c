00115410396ed225c4962aa15a094f0d1d694d06
saintgokex <drgokex@gmail.com>
2022-09-20 08:12:05 -0700

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int a = 0, lend = 0;
+	char temp;
+
+	while (s[a++])
+		len++;
+
+	for (a = len - 1; a >= len / 2; a--)
+	{
+		temp = s[a];
+		s[a] = s[len - a - 1];
+		s[len - a - 1] = temp;
+	}
+}
