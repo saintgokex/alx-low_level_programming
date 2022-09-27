bd93a661601c95e87d6f16f40f69dbb03fc91866
saintgokex <drgokex@gmail.com>
2022-09-27 00:36:12 -0700

    copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,10 +8,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int a;
 
-	for (i = 0; i < n; i++)
-		*(dest + i) = *(scr + i);
+	for (a = 0; a < n; a++)
+		*(dest + a) = *(scr + a);
 
 	return (dest);
 }
