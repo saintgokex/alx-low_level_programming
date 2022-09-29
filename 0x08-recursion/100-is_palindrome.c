981e4151f3fa0aebc54446fb746d676dfde0e9e5
saintgokex <drgokex@gmail.com>
2022-09-29 23:43:44 +0100

    shows if a string is empty

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string
+ * Return: 1 if empty, 0 if not
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * comparator - compares each character of the string
+ * @s: string
+ * @n1: smallest iterator
+ * @n2: biggest iterator
+ * Return: .
+ */
+int comparator(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
