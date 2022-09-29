e32eab9ced57cb512b6ddbdb76801deaedca3aee
saintgokex <drgokex@gmail.com>
2022-09-29 23:01:38 +0100

    prints the factorial of a given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * factorial - gets the factorial of a given numner
- * @n: integer 
- * Return: returns -1 if @n is lower than 0 or the facrorial of the number if otherwise
+ * @n: integer
+ * Return: -1 if n is less than 0, factorial is otherwise
  */
 int factorial(int n)
 {
 	if (n < 0)
-		return(-1);
+		return (-1);
 	else if (n <= 1)
-		return(1);
+		return (1);
 	else
-		return(n * factorial(n -1));
+		return (n * factorial(n - 1));
 }
