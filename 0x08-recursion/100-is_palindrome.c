b7380ed1ee4f6aa7a49b72394662fd7bcaa576c0
saintgokex <drgokex@gmail.com>
2022-09-29 23:49:10 +0100

    detects if a string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,12 +2,12 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: the string
- * Return: 1 if empty, 0 if not
+ * Return: the length of a string
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return (1);
+		return (0);
 	else
 		return (1 + _strlen_recursion(s + 1));
 }
@@ -28,4 +28,16 @@ int comparator(char *s, int n1, int n2)
 		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
+}
+
+/**
+ * is_palindrome - detects if a string is a palindrome
+ * @s: string
+ * Return: 1 if s is a palindrome, 0 if not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
