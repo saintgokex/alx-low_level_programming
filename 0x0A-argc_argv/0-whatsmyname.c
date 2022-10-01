70d15c009a5343c2e1a39990995292c82ecc6cb0
saintgokex <drgokex@gmail.com>
2022-10-01 10:00:12 +0100

    print first argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * argc: argument count
+ * *argv: argument vector
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	for (argv[] = 0; argv[] <= (argc - 1); argv[]++)
+	{
+		_putchar("argv[0]");
+	}
+	_putchar('\n');
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this is the argc and argv directory
