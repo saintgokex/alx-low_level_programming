c9e0940e314ff9bfd3fca4270cb503518550fc59
saintgokex <drgokex@gmail.com>
2022-10-01 10:05:41 +0100

    prints the first command argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - entry point
- * argc: argument count
- * *argv: argument vector
+ * @argc: argument count
+ * @argv: argument vector
+ *
  * Return: 0
  */
-int main (int argc __attribute__((unused)), char *argv[])
+
+int main(int argcs __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 
