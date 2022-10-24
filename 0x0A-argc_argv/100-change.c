31f726cc17105f4dcc67b5f8901a188640f1b95d
saintgokex <drgokex@gmail.com>
2022-10-24 14:20:55 +0100

    list header file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,6 +4,7 @@
  * main - prints the minimum number of coins to make change
  * @argc: number of command line arguments
  * @argv: array that contains the program command line arguments
+ *
  * Return: 0
  */
 int main(int argc, char *argv[])
