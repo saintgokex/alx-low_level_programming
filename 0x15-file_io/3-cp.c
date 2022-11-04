46298bbb6880ccbacf4019e1f128bfc455397b9b
saintgokex <drgokex@gmail.com>
2022-11-04 09:10:14 +0100

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
 	while (nchars == 1024)
 	{
 		nchars = read(file_from, buf, 1024);
-		if (nchars == 1)
+		if (nchars == -1)
 			error_file(-1, 0, argv);
 		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
